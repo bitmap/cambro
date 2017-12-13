2fa57068bf9c4cb98a8eca6bfefb9acdf6a1d065
Cabe Branson <cabe.branson@b-reel.com>
2017-12-13 14:52:47 -0500

    remove counter from contructor, improve performance

--- src/cambro.js
+++ src/cambro.js
@@ -3,7 +3,6 @@
 const bro = Symbol('bro')
 const draw = Symbol('draw')
 const normalize = Symbol('normalize')
-const count = Symbol('count')
 
 class Cambro {
   constructor (x, y, t = 128, w = 640, h = 480) {
@@ -15,7 +14,6 @@ class Cambro {
     this.showGuides = true
     this.data = []
     this.socket = io.connect()
-    this.index = -1
   }
 
   get totalSlices () {
@@ -36,14 +34,6 @@ class Cambro {
     return this.slice.width * this.height / this.gridY
   }
 
-  [count] (val) {
-    this.index += val
-
-    if (this.index >= this.totalSlices) {
-      this.index = 0
-    }
-  }
-
   [normalize] (value) {
     const average = value / 3
     const normied = Math.floor(average / this.maxPixels * 100)
@@ -51,10 +41,14 @@ class Cambro {
   }
 
   [bro] (source, context, backContext) {
+    let sliceValue = 0
+    let sliceCount = -1
+    let cambroData = []
+
     for (let iy = 0; iy < this.gridY; iy += 1) {
       for (let ix = 0; ix < this.gridX; ix += 1) {
         // This is the current slice number
-        this[count](1)
+        sliceCount += 1
 
         // Find position of this slice
         const start = this.slice.width * ix
@@ -91,14 +85,11 @@ class Cambro {
         }
 
         // calculate average of lightness
-        const normie = this[normalize](lightness)
+        sliceValue = this[normalize](lightness)
 
         // update data array
-        this.data.splice(this.counter, 1, normie)
-        const cambroData = this.data
-
-        // send data over websockets
-        this.socket.emit('cambro_data_in', { cambroData, undefined })
+        this.data.splice(sliceCount, 1, sliceValue)
+        cambroData = this.data
 
         // draw image to canvas
         context.putImageData(sliceImage, start, end)
@@ -106,13 +97,16 @@ class Cambro {
         if (this.showGuides) {
           context.font = '20px monospace'
           context.fillStyle = 'lime'
-          context.fillText(normie.toString(), start + 2, end + 20)
+          context.fillText(sliceValue.toString(), start + 2, end + 20)
 
           context.fillStyle = 'magenta'
-          context.fillText(this.index.toString(), start + 2, end + 40)
+          context.fillText(sliceCount.toString(), start + 2, end + 40)
         }
       }
     }
+
+    // send data over websockets
+    this.socket.emit('cambro_data_in', { cambroData, undefined })
   }
 
   [draw] (video, context, backContext) {
@@ -152,5 +146,5 @@ class Cambro {
 }
 
 const cambro = new Cambro(4, 4)
-
+console.log(cambro);
 cambro.start()
